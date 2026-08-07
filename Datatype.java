5d082bafad2630204e866ce1609cd74e53cf2dee
selvamdivya <selvamdivya177@gmail.com>
2026-08-07 21:41:09 +0530

    "Datatype program"

---
+++ Datatype.java
@@ -0,0 +1,12 @@
+public class Datatype {
+    public static void main(String[] args){
+        String name = "Divya";
+        int age = 19;
+        float marks = 92.5f;
+
+        System.out.println("Name:" +name);
+        System.out.println("Age:" +age);
+        System.out.println("Marks:" +marks);
+    }
+    
+}
