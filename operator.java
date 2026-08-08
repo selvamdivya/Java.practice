d303c05e03616967c94c0fd66bd347888d3f81b0
selvamdivya <selvamdivya177@gmail.com>
2026-08-08 19:31:59 +0530

    "operator program"

---
+++ operator.java
@@ -0,0 +1,11 @@
+public class operator {
+    public static void main(String[] args) {
+        int a = 10, b = 2;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+    
+}
