ab340b674eac0168e43e35766ac365f7547fd858
selvamdivya <selvamdivya177@gmail.com>
2026-08-09 21:54:03 +0530

    "IfElse program"

---
+++ IfElseExample.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num >= 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is negative.");
+        }
+
+        sc.close();
+    }
+}
