8166668586fb641c4e6dfe58f2030d7b01a904d3
selvamdivya <selvamdivya177@gmail.com>
2026-08-11 21:17:27 +0530

    "Swaping program"

---
+++ swap.java
@@ -0,0 +1,15 @@
+class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
