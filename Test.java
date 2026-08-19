a5efd14b23a657ce17d4a0567a58e1fb39b0f39b
selvamdivya <selvamdivya177@gmail.com>
2026-08-19 18:52:57 +0530

    "use trycatch program"

---
+++ Test.java
@@ -0,0 +1,16 @@
+class Test {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10;
+            int b = 0;
+
+            System.out.println(a / b);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+
+        System.out.println("Program continues...");
+    }
+}
